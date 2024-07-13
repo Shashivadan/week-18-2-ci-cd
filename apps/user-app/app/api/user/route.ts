c3775965bb462d849c87f03fb341e67b2072ec58
ThotaShashivadan <168289227+ThotaShashivadan@users.noreply.github.com>
2024-07-13 12:45:21 -0400

    Update route.ts

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,6 +3,14 @@ import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
+
+
+
+
+
+
+
+    
     const session = await getServerSession(authOptions);
     if (session.user) {
         return NextResponse.json({
